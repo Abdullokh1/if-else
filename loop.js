7e24b7486a530d494df7c0355c1fc5f8201b93ad
Abdullokh1 <giyasovabdullo0@gmail.com>
2022-03-03 09:00:36 +0500

    JS

---
+++ loop.js
@@ -0,0 +1,166 @@
+// let n = 50;
+// let s = 0;
+
+// for(let i = 1; i<=n; i++){
+//   s+=1/i;
+// }
+// console.log(s);
+
+
+
+// FOR 1
+
+// let k = 10;
+// let n = 5;
+
+// for(let i = 0; i < n; i++){
+//   console.log(k);
+// }
+
+
+// FOR 2
+
+// let a = 10;
+// let b = 20;
+// for (let i = a; i <=b; i++){
+//   console.log(i);
+// }
+
+
+// FOR 3 
+
+// let a = 10;
+// let b = 20;
+// for (let i = b-1; i > a; i--){
+//   console.log(i);
+// }
+
+// FOR 4
+
+// let price = 5000;
+
+// for (let i = 1; i <= 10; i+=1){
+//  console.log(`Konfet narxi: ${i} kg: ${i*price} so'm`);
+// }
+
+
+// FOR 5
+
+// let price = 5000;
+
+// for (let i = 0.1; i <= 1; i+=0.1){
+//  console.log(`Konfet narxi: ${i.toFixed(1)} kg: ${i.toFixed(2)*price} so'm`);
+// }
+
+
+// FOR 6
+
+// let price = 5000;
+
+// for (let i = 1.2; i <= 2; i+=0.2){
+//  console.log(`Konfet narxi: ${i.toFixed(1)} kg: ${i.toFixed(2)*price} so'm`);
+// }
+
+
+// FOR 7
+
+// let a = 10;
+// let b = 20;
+
+// for (let i = a; i <= b; i++){
+//   console.log(i+i);
+// }
+
+
+// FOR 8
+
+// let a = 5;
+// let b = 10;
+
+// for (let i = a; i <= b; i++){
+//   console.log(i*i);
+// }
+
+
+
+// FOR 9
+
+// let a = 2;
+// let b = 10;
+
+// let sum = 0;
+
+// for (let i = a; i <= b; i++){
+//   sum+= i**2;
+// }
+// console.log(sum);
+
+
+// FOR 10
+
+// let n = 10;
+// let s = 0;
+
+// for(let i = 1; i <= n; i++){
+//   s += 1+ 1/i + 1/n;
+// }
+//   console.log(s);
+
+
+// FOR 11
+
+// let n = 10;
+// let s = 0;
+
+// for(let i = 1; i <= n; i++){
+//   s = n**2+(n+i)**2+(n+i+1)**2 +(2*n)**2;
+// }
+// console.log(s);
+
+
+// FOR 12
+
+// let n = 2;
+// let s = 0;
+
+// for(let i = 1.1; i <= n; i+=0.1){
+//   s = i*i;
+// }
+// console.log(s.toFixed(1));
+
+
+// FOR 14
+
+// let n = 10;
+// for (let i = 1 ; i <= n; i += 2 ) {
+//   console.log(i ** 2)
+// }
+
+
+// FOR 17
+
+// let a = 2;
+// let n = 4;
+// let sum = 0;
+// for(let i = 1; i <= n; i++) {
+//   sum += a**i;
+// }
+// console.log(sum);
+
+
+// FOR 19
+
+// let a = 5;
+// let c = 1;
+// for(i = 1; i <= a; i++) {
+//   c *= i;
+// }
+// console.log(c);
+
+
+
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,149 @@
+// let a = +prompt('a: ');
+// let b = +prompt('b: ');
+// let c = +prompt('c: ');
+
+// if (a > b && a<c){
+//   console.log(a);
+// }else if (b > a && b<c){
+//   console.log(b);
+// }
+// else if (a<b && b<c){
+//   console.log(b);
+// }
+// else if(b<a && b<c){
+//   console.log(b);
+// }
+// else{
+//   console.log(c);
+// }
+
+
+// let a = +prompt('a: ');
+// let b = +prompt('b: ');
+
+// if (a==b){
+//   console.log('0');
+// }
+// else if (a>b){
+//   console.log(a);
+// }
+// else if (b>a){
+//   console.log(b);
+// }
+
+
+// let x = +prompt('x: ');
+// let fx;
+
+// if (x>0){
+//   fx = 2*Math.sin(x)
+// }
+// else{
+//   fx = x-6;
+// }
+// console.log(fx.toFixed(1));
+
+
+
+// let x = +prompt('x: ');
+// let fx;
+
+// if (x < -2 || x>2){
+//   fx = 2*x;
+// }
+// else{
+//   fx = -3*x;
+// }
+// console.log(fx);
+
+
+
+// let a = +prompt('a: ');
+// let b = +prompt('b: ');
+// let c = +prompt('c: ');
+
+// if (a+b > c && a+c > b && b+c>a){
+//   console.log('Yes');
+// }
+// else {
+//   console.log("no");
+// }
+
+
+// 9 - EXERCISE 
+
+// let temp = +prompt('Temp: ');
+
+// if (temp <= 0){
+//   console.log('Freezing');
+// }
+// else if (temp > 0 && temp <10){
+//   console.log('Very Cold');
+// }
+// else if (temp > 10 && temp <=20){
+//   console.log('Cold');
+// }
+// else if (temp > 20 && temp <=30){
+//   console.log('Normal');
+// }
+// else if (temp > 30 && temp <= 40){
+//   console.log('Hot');
+// }
+// else if (temp > 40){
+//   console.log('Very Hot');
+// }
+
+
+// 10 - EXERCISE 
+
+// let a = +prompt('a: ');
+
+// if (a > 0 && a % 2 !=0){
+//   console.log('Musbat toq son');
+// }
+// else if (a < 0 && a % 2 == 0){
+//   console.log('Manfiy juft son');
+// }
+// else if (a > 0 && a % 2 ==0){
+//   console.log('Musbat juft son');
+// }
+// else if (a < 0 && a % 2 !=0){
+//   console.log('Manfiy toq son');
+// }
+// else{
+//   console.log('0 ga teng emas');
+// }
+
+
+// 11 - EXERCISE 
+
+
+// let a = +prompt('a: ');
+
+// if (a > 0 && a <= 99 && a % 2 == 0){
+//   console.log('Ikki xonali juft son');
+// }
+// else if (a > 0 && a <= 99 && a % 2 == 1){
+//   console.log('Ikki xonali toq son');
+// }
+// else if (a >= 100 && a <= 999 && a % 2 == 0){
+//   console.log('Uch xonali juft son');
+// }
+// else if (a >= 100 && a<=999  && a % 2 == 1){
+//   console.log('Uch xonali toq son');
+// }
+// else{
+//   console.log('error');
+// }
+
+
+// 12 - EXERCISE 
+
+// let n = +prompt('n: ');
+
+// if (n>0){
+//   console.log(n**2);
+// }
+// else{
+//   console.log('0');
+// }
